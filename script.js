03fb87849a766d7be2c1bf30244c0b018e7e94a5
NekoAprendiz <andaluz121@gmail.com>
2024-09-20 00:43:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('September 11, 2024 20:32:00').getTime();
+    const countDate = new Date('October 5, 2024 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
